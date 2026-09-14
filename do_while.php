70720079cb87c84dfc016df2966679d2143855d2
Vanya Monzón <vanyacmn@gmail.com>
2026-09-13 22:42:21 -0300

    Add files via upload

---
+++ do_while.php
@@ -0,0 +1,18 @@
+<?php
+
+$c=14;
+$num=5;
+
+do{
+    echo $num. "X ".$c." = ".$num*$c."<br>";
+    $c--;
+}while($c>=1);
+
+
+$w=1;
+$num2=3;
+
+do{
+    echo $num2. "X ".$w." = ".$num2*$w."<br>";
+    $w++;
+}while($w<=22);
